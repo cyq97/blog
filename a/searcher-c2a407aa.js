666bd3b827e4db3646a6c52a05b676aa60e199a6
cyq97 <cyq97@users.noreply.github.com>
2026-04-21 15:09:14 +0000

    deploy: c4a9117d6bf6e06bfa2e4d51f2f071ac5fbc1ec3

--- a/searcher-c2a407aa.js
+++ a/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-640e0f4c.js',
+                path_to_root + 'searchindex-27ebf0ef.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
